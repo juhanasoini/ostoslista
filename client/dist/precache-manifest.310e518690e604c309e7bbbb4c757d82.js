c3a9ffb66aa616c17fbc775e725fa0bc7e208756
Juhana Soini <juhana.soini@avenis.fi>
2020-02-03 10:17:08 +0200

    Included client/dist

---
+++ client/dist/precache-manifest.310e518690e604c309e7bbbb4c757d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec563c6d1a064cb473b",
+    "url": "/css/app.ccc1fd5f.css"
+  },
+  {
+    "revision": "01a7700d81ab5f6fba88",
+    "url": "/css/chunk-vendors.fbe9bade.css"
+  },
+  {
+    "revision": "c2eae80bc7b067f6b81562fd69f0ca05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cec563c6d1a064cb473b",
+    "url": "/js/app.af6f53c1.js"
+  },
+  {
+    "revision": "01a7700d81ab5f6fba88",
+    "url": "/js/chunk-vendors.7eb3d863.js"
+  },
+  {
+    "revision": "0be51931a11b5262cbc54306209391c0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
